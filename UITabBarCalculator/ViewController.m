0f5d2511d69adcb02a857a2e676adf6d1f131bb4
Volodymyr Poplavskyi <poplavskiy.v1987@gmail.com>
2017-04-07 22:21:16 +0300

    Uploading very simple UITabBar Calculator

---
+++ UITabBarCalculator/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  UITabBarCalculator
+//
+//  Created by Students on 02.12.15.
+//  Copyright © 2015 CS193p. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (strong, nonatomic) IBOutlet UITextField *arg1AddTextField;
+@property (strong, nonatomic) IBOutlet UITextField *arg2AddTextField;
+@property (strong, nonatomic) IBOutlet UILabel *resultAddLabel;
+@property (strong, nonatomic) NSMutableArray* array;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+// Hiding keyboard when press the button "Hide Keyboard"
+- (IBAction)hideKeyboradButtonAdd:(UIButton *)sender {
+    [self.view endEditing:YES];
+}
+
+// Makes adding two input arguments and showing the result by label
+- (IBAction)calculateAdd:(UIButton *)sender {
+    NSUInteger result =[_arg1AddTextField.text intValue] + [_arg2AddTextField.text intValue];
+    _resultAddLabel.text = [NSString stringWithFormat:@"%lu",(unsigned long)result];
+}
+
+
+
+
+
+
+
+@end
